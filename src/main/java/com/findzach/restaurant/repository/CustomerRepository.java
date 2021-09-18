ac31ad6b40582e8a8b42f33906b1676613cb3f61
FindZach <zach@findzach.com>
2021-09-18 01:45:50 -0400

    Created Customer Repository closes #9

---
+++ src/main/java/com/findzach/restaurant/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.findzach.restaurant.repository;
+
+import com.findzach.restaurant.model.customer.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Zach S <zach@findzach.com>
+ * @since 9/17/2021
+ */
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
