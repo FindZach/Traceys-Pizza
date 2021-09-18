811b870a834b660f046e1735ba8a2c412da8dc9b
FindZach <zach@findzach.com>
2021-09-18 01:09:38 -0400

    Created CustomerController closes #3

---
+++ src/main/java/com/findzach/restaurant/controller/rest/CustomerController.java
@@ -0,0 +1,11 @@
+package com.findzach.restaurant.controller.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Zach S <zach@findzach.com>
+ * @since 9/17/2021
+ */
+@RestController
+public class CustomerController {
+}
