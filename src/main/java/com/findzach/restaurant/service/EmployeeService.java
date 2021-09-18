92c91d9f5daa7c21793a8e2392f3bee998e5d08f
FindZach <zach@findzach.com>
2021-09-18 01:38:21 -0400

    Employee Service created closes #4

---
+++ src/main/java/com/findzach/restaurant/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.findzach.restaurant.service;
+
+import com.findzach.restaurant.model.employee.Employee;
+
+/**
+ * @author Zach <zach@findzach.com>
+ * @since 9/17/2021
+ */
+public interface EmployeeService extends CrudService<Employee, Long> {
+
+}
