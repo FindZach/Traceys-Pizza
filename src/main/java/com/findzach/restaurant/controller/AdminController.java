a2204799c2388b73f9046c7a50c3068f417560d0
Zach <zach@findzach.com>
2023-11-23 22:21:37 -0500

    updating thymeleaf

--- src/main/java/com/findzach/restaurant/controller/AdminController.java
+++ src/main/java/com/findzach/restaurant/controller/AdminController.java
@@ -1,8 +1,12 @@
 package com.findzach.restaurant.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Zach S <zach@findzach.com>
@@ -12,11 +16,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
+    boolean isAdmin = true;
 
     @GetMapping("/admin")
-    public String viewAdminPage() {
+    public String viewAdminPage(HttpServletRequest request) {
 
+        HttpSession session = request.getSession();
+        session.setAttribute("authenticated", true);
+
+        System.out.println("User Session Data: " + session.getId());
+        System.out.println("User Session Data: " + session.getAttribute("authenticated"));
+        if (!isAdmin) {
+            return "404";
+        }
+        System.out.println("New Updates!");
         return "admin";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(Model model, HttpServletRequest request) {
+        // Add dynamic data to the model
+        HttpSession session = request.getSession();
+        session.setAttribute("authenticated", true);
+
+        System.out.println("User Session Data: " + session.getId());
+        System.out.println("User Session Data: " + session.getAttribute("authenticated"));
+        model.addAttribute("message", "Hello, Spring Boot and Thymeleaf!");
+        return "/greeting"; // This corresponds to the template name "greeting.html"
     }
 
 }
--- src/main/java/com/findzach/restaurant/model/Role.java
+++ src/main/java/com/findzach/restaurant/model/Role.java
@@ -13,6 +13,7 @@ public enum Role {
     EMPLOYEE(),
     MANAGER(),
     ADMIN(),
+    OWNER(),
     DEVELOPER()
 
 }
