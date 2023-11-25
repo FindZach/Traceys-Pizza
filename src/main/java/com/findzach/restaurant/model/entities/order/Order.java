788824f28d607b7e9de849771de85211aa6dec23
Zach <zach@findzach.com>
2023-11-24 19:24:53 -0500

    Removed legacy User interface

--- src/main/java/com/findzach/restaurant/model/entities/order/Order.java
+++ src/main/java/com/findzach/restaurant/model/entities/order/Order.java
@@ -1,8 +1,8 @@
 package com.findzach.restaurant.model.entities.order;
 
 import com.findzach.restaurant.model.BaseEntity;
+import com.findzach.restaurant.model.entities.user.User;
 import com.findzach.restaurant.model.entities.user.employee.Employee;
-import com.findzach.restaurant.model.menu.User;
 
 /**
  * @author Zach S <zach@findzach.com>
