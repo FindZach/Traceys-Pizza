22b33924a486263f07f3ab42d2aa23c4f73787ed
Zach <zach@findzach.com>
2023-11-24 20:34:07 -0500

    Updated JPA & Added Getters/Setters

--- src/main/java/com/findzach/restaurant/model/entities/Item.java
+++ src/main/java/com/findzach/restaurant/model/entities/Item.java
@@ -3,10 +3,9 @@ package com.findzach.restaurant.model.entities;
 import com.findzach.restaurant.model.BaseEntity;
 import com.findzach.restaurant.model.menu.MenuType;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.MappedSuperclass;
-import java.awt.*;
-import java.util.List;
-import java.util.Set;
 
 /**
  * @author Zach S <zach@findzach.com>
@@ -20,9 +19,50 @@ public abstract class Item extends BaseEntity {
     private String itemName;
     private String description;
 
-    private Set<MenuType> menuTypes;
+    @Enumerated(EnumType.STRING)
+    private MenuType menuType;
 
     private double price;
     //The amount of this item
     private int quantity;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public MenuType getMenuType() {
+        return menuType;
+    }
+
+    public void setMenuType(MenuType menuType) {
+        this.menuType = menuType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
--- src/main/java/com/findzach/restaurant/model/entities/food/FoodItem.java
+++ src/main/java/com/findzach/restaurant/model/entities/food/FoodItem.java
@@ -2,10 +2,13 @@ package com.findzach.restaurant.model.entities.food;
 
 import com.findzach.restaurant.model.entities.Item;
 
+import javax.persistence.Entity;
+
 /**
  * @author: Zach Smith
  * @date: 11/24/2023
  * @time: 8:02 PM
  */
+@Entity
 public class FoodItem extends Item {
 }
