6b6c52e5d4d01480a71a82c2e2cc8b5758a0f3d7
Zach <zach@findzach.com>
2023-11-26 14:02:50 -0500

    Added Constants  | WebStatus object | under-maintenance.html

---
+++ src/main/java/com/findzach/restaurant/config/WebStatus.java
@@ -0,0 +1,12 @@
+package com.findzach.restaurant.config;
+
+/**
+ * @author: Zach Smith
+ * @date: 11/26/2023
+ * @time: 1:52 PM
+ */
+public enum WebStatus {
+    PRODUCTION(),
+    TESTING(),
+    UNDER_MAINTENANCE()
+}
