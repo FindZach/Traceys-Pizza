345d6564b68172550712c119d338c2abd2b7805a
Zach <zach@findzach.com>
2023-12-02 20:54:45 -0500

    adjusted footer.html & added contact-us.html

---
+++ src/main/java/com/findzach/restaurant/controller/ContactController.java
@@ -0,0 +1,31 @@
+package com.findzach.restaurant.controller;
+
+import com.findzach.restaurant.controller.pages.PizzaPage;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author: Zach Smith
+ * @date: 12/2/2023
+ * @time: 8:47 PM
+ */
+@RequestMapping("contact")
+@Controller
+public class ContactController implements PizzaPage {
+    /**
+     * @param model
+     * @param httpServletRequest
+     * @return
+     */
+    @GetMapping
+    @Override
+    public String showPage(Model model, HttpServletRequest httpServletRequest) {
+        setDefaults(model);
+
+        return "contact-us";
+    }
+}
