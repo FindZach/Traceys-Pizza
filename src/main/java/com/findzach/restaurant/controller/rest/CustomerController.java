8803b0de87b3fbed0e49ba15e94ed5ceda8ac5ea
Zach <zach@findzach.com>
2023-12-03 11:50:00 -0500

    Refactored Navbar

--- src/main/java/com/findzach/restaurant/controller/rest/CustomerController.java
+++ src/main/java/com/findzach/restaurant/controller/rest/CustomerController.java
@@ -6,6 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Zach S <zach@findzach.com>
  * @since 9/17/2021
  */
-@RestController
 public class CustomerController {
 }
--- src/main/java/com/findzach/restaurant/controller/rest/EmployeeController.java
+++ src/main/java/com/findzach/restaurant/controller/rest/EmployeeController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Zach S <zach@findzach.com>
  * @since 9/17/2021
  */
-@RestController
 public class EmployeeController {
 
     @Autowired
